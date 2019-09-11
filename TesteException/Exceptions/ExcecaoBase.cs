e18c1c0ed41259518fc7790aeaf1dd9a4371debb
João Fonseca <joaomatheus_fonseca@hotmail.com>
2019-09-10 23:38:09 -0300

    exception global com excecao customizada e gravacao de log

---
+++ TesteException/Exceptions/ExcecaoBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteException.Exceptions
+{
+    public class ExcecaoBase : Exception
+    {
+        public string CodigoErro { get; protected set; }
+        public string Mensagem { get; protected set; }
+        public object Data { get; protected set; }
+
+        public ExcecaoBase()
+        {
+            
+        }
+    }
+
+    public class ExcecaoErroAoGerarPedido : ExcecaoBase
+    {
+        public ExcecaoErroAoGerarPedido()
+        {
+            CodigoErro = "erro_ao_gerar_pedido";
+            Mensagem = "Erro na geração do pedido";
+        }
+    }
+}
---
+++ TesteException/Exceptions/ExceptionHandlerExtensions.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace TesteException.Exceptions
+{
+    public static class ExceptionHandlerExtensions
+    {
+        public static void UseGlobalExceptionHandler(this IApplicationBuilder app, ILoggerFactory loggerFactory, string NomeAplicacao)
+        {
+            app.UseExceptionHandler(builder =>
+            {
+                builder.Run(async context =>
+                {
+                    var exceptionHandlerFeature = context.Features.Get<IExceptionHandlerFeature>();
+
+                    if (exceptionHandlerFeature != null)
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        context.Response.ContentType = "application/json";
+
+                        RetornoExcecao retornoExcecao = null;
+
+                        if (exceptionHandlerFeature.Error is ExcecaoBase)
+                        {
+                            var excecaoBase = exceptionHandlerFeature.Error as ExcecaoBase;
+
+                            retornoExcecao = new RetornoExcecao
+                            {
+                                CodigoErro = excecaoBase.CodigoErro,
+                                Mensagem = excecaoBase.Mensagem
+                            };
+                        }
+                        else
+                        {
+                            retornoExcecao = new RetornoExcecao
+                            {
+                                CodigoErro = "erro_nao_tratado",
+                                Mensagem = "Ocorreu um erro não tratado"
+                            };
+                        }
+
+                        var logger = loggerFactory.CreateLogger("GlobalExceptionHandler");
+
+                        var objetoLog = new
+                        {
+                            CodigoErro = retornoExcecao.CodigoErro,
+                            Mensagem = retornoExcecao.Mensagem,
+                            MensagemException = exceptionHandlerFeature.Error.Message,
+                            TrexoErro = exceptionHandlerFeature.Error.StackTrace,
+                            ExcecaoInterna = exceptionHandlerFeature.Error.InnerException
+                        };
+
+                        logger.LogError(exceptionHandlerFeature.Error, JsonConvert.SerializeObject(objetoLog), new object[0]);
+
+                        await context.Response.WriteAsync(JsonConvert.SerializeObject(retornoExcecao));
+                    }
+                });
+            });
+        }
+    }
+
+    public class RetornoExcecao
+    {
+        public string CodigoErro { get; set; }
+        public string Mensagem { get; set; }
+    }
+}
